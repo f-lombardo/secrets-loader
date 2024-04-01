088dc2097d0bb9fff80dbc4db50d61c57cbc7f9f
Franco Lombardo <f.lombardo69@gmail.com>
2024-04-01 19:27:52 +0200

    Fix github workflow - 2

--- scripts/quality.sh
+++ scripts/quality.sh
@@ -4,4 +4,6 @@
 # This script runs quality checks
 ########################################################################################################################
 
+echo "Project: ${COMPOSE_PROJECT_NAME}"
+
 vendor/bin/phpcbf && vendor/bin/phpcs && vendor/bin/phpstan && vendor/bin/phpunit
